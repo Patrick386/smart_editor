2084f43538395fd390166c5615e4f32622edc8d8
patrick386 <patrick386@sint.kr>
2022-04-19 22:25:27 +0900

    Initial commit

---
+++ lib/smart_editor.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class SmartEditor extends StatefulWidget {
+  const SmartEditor({Key? key}) : super(key: key);
+
+  @override
+  State<SmartEditor> createState() => _SmartEditorState();
+}
+
+class _SmartEditorState extends State<SmartEditor> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
